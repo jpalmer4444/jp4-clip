960c44427cdf11214d4dd2a5036579e107b3097a
Jason Palmer <jpalmer@itemmaster.com>
2016-12-21 09:10:32 -0600

    Syncing from way back.

--- index.js
+++ index.js
@@ -4,11 +4,14 @@
  * and open the template in the editor.
  */
 let LOG = require('./lib/logging').getLogger();
-        //TiffByteReader = require("./lib/TiffByteReader").TiffByteReader,
-        let TiffPSByteReader = require("./lib/TiffPSByteReader").TiffPSByteReader;
-        let LOCAL = require("./lib/local");
+//TiffByteReader = require("./lib/TiffByteReader").TiffByteReader,
+let TiffPSByteReader = require("./lib/TiffPSByteReader").TiffPSByteReader;
+let PSTiffImageWriter = require("./lib/PSTiffImageWriter").PSTiffImageWriter;
+let LOCAL = require("./lib/local");
 
-let localize = function(event, context, CALLBACK){
+//(header, ifd0, inputfilename, outputfilename, callback)
+
+let localize = function (event, context, CALLBACK) {
     return LOCAL.handler(event, context, CALLBACK);
 };
 
@@ -20,42 +23,49 @@ exports.handler = function (event, context, CALLBACK) {
     event = local.event;
     context = local.context;
     CALLBACK = local.CALLBACK;
-    
+
     let path = local.event['TEST_RESIZE_3_TIF'];
-    let filename = path.substring(path.lastIndexOf('/')+1, path.length);
-    let directory = path.substring(0, path.lastIndexOf('/')+1);
-    
+    let filename = path.substring(path.lastIndexOf('/') + 1, path.length);
+    let directory = path.substring(0, path.lastIndexOf('/') + 1);
+
     //let tiffByteReader = new TiffByteReader();
     let tiffByteReader = new TiffPSByteReader(filename, directory);
     let ifdOffset = event.ifdOffset ? ifdOffset : 0;
-    tiffByteReader.read(ifdOffset, path, function(err, ifd0){
-        if(err){
+    tiffByteReader.read(ifdOffset, path, function (err, ifd0) {
+        if (err) {
             LOG.error(JSON.stringify(err), err.stack);
             CALLBACK(err, err.stack);
             process.exit();
-        }else{
+        } else {
             let photoshopImageResourceBlock = ifd0.getEntry(34377);
-            if(photoshopImageResourceBlock){
-                let clippingPathPoints = photoshopImageResourceBlock.value.getPhotoshopEntry(2000).resources;
-                
-                LOG.info('Found Points!');
-            }
-            if(ifd0.ifdOffset){
-                event.ifdOffset = ifd0.ifdOffset;
-                iterations++;
-                if(iterations < iterationLimit){
-                    
-                    LOG.info('Reading next IFD');
-                    exports.handler(event, context, CALLBACK);
-                }
-            }else{
-                LOG.info('Finished reading TIF');
-                process.exit();
+            if (photoshopImageResourceBlock) {
+                //let clippingPathPoints = photoshopImageResourceBlock.value.getPhotoshopEntry(2000).resources;
+                let tiffWriter = new PSTiffImageWriter(tiffByteReader.header, ifd0, path, '/tmp/test.tif', function (err, results) {
+                    if (err) {
+                        LOG.error(err);
+                        CALLBACK(err, err.stack);
+                        process.exit();
+                    } else {
+                        if (ifd0.ifdOffset) {
+                            event.ifdOffset = ifd0.ifdOffset;
+                            iterations++;
+                            if (iterations < iterationLimit) {
+
+                                LOG.info('Reading next IFD');
+                                exports.handler(event, context, CALLBACK);
+                            }
+                        } else {
+                            LOG.info('Finished reading TIF');
+                            process.exit();
+                        }
+                    }
+                });
+                //LOG.info('Found Points!');
             }
         }
     });
-    
-    
+
+
 };
 
 //to run from command line.
